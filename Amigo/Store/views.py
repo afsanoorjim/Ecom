b989f39e688e34c7e2e3e06fbd4b254c0cc7cd65
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-08-10 05:35:37 +0000

    commited

--- Amigo/Store/views.py
+++ Amigo/Store/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import *
+from .form import RegisterUser
+from django.contrib import messages
+
+
 
 def landing(request):
     products = Product.objects.all()
+    if request.POST == 'POST':
+        form = RegisterUser(request.POST)
+        if form.is_valid:
+            form.save()
+            messages.success('User Created')
+            return render(request, 'index.html', context)
+    else:
+        form = RegisterUser()
+
+
     context = {
-        'products': products
+        'products': products, 
+        'form':form,
     }
     return render(request, 'index.html', context)
