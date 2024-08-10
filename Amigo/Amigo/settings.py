6f3c05348136a3c90d9e66b08c46449d3e2862e2
Afsan Noor Jim <150720347+afsanoorjim@users.noreply.github.com>
2024-08-10 08:41:49 +0000

    commited

--- Amigo/Amigo/settings.py
+++ Amigo/Amigo/settings.py
@@ -24,13 +24,9 @@ SECRET_KEY = 'django-insecure-6!3n%h4v^wu5to)^tce9%c+-ok^1q9)*#fk85d#zcqje6-11c^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-import os
-from decouple import config
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
-if 'CODESPACE_NAME' in os.environ:
-    codespace_name = config("CODESPACE_NAME")
-    codespace_domain = config("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
-    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'https://*.github.dev']
+
 
 # Application definition
 
